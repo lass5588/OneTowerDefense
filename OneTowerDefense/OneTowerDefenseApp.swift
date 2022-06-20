fdcb8f0db2695478045ecae037d35f7883a1f5b6
Lasse <lass5588@icloud.com>
2022-06-20 12:00:50 +0200

    Initial Commit

---
+++ OneTowerDefense/OneTowerDefenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneTowerDefenseApp.swift
+//  OneTowerDefense
+//
+//  Created by Lasse Andersen on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct OneTowerDefenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
