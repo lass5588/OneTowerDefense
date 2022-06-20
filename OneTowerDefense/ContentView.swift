69b015d5cc76bb47ce1d3403366962fe74578eb9
Lasse <lass5588@icloud.com>
2022-06-20 22:43:09 +0200

    functionality for collision

--- OneTowerDefense/ContentView.swift
+++ OneTowerDefense/ContentView.swift
@@ -6,14 +6,48 @@
 //
 
 import SwiftUI
+import SpriteKit
 
 struct ContentView: View {
+    
+    @State var xPosPlayer: CGFloat = 200
+    @State var yPosPlayer: CGFloat = 300
+    
+    @State var xPosEnemy: CGFloat = 200
+    @State var yPosEnemy: CGFloat = 600
+    
+    @State var counter = 0
+    
+    
     var body: some View {
-        VStack{
-            Text("Hello, World!")
-                .padding()
+        ZStack{
+            
+            Circle()
+                .fill(Color.blue)
+                .frame(width: 100, height: 100)
+                .position(x: xPosPlayer, y: yPosPlayer)
+            
+            Rectangle()
+                .fill(Color.red)
+                .frame(width: 100, height: 50)
+                .position(x: xPosEnemy, y: yPosEnemy)
+                .gesture(DragGesture().onChanged({value in
+                    self.xPosEnemy = value.location.x
+                    self.yPosEnemy = value.location.y
+                    self.checkCollision()
+                }))
+            
+            
         }
     }
+    
+    func checkCollision(){
+        if abs(self.xPosEnemy - self.xPosPlayer) < 100 && abs(self.yPosEnemy - self.yPosPlayer) < 100{
+            counter += 1
+            print("A collision has occured \(counter) number of times.")
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
